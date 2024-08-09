6f416d0dd92f0062efb43d8afb7e766ff7343f79
Karan Thakker <karan17thakkar@gmail.com>
2024-08-09 17:15:10 +0800

    Creating all Use-Cases

---
+++ install-all-env.bat
@@ -0,0 +1,10 @@
+oc create project oc-testenv
+oc create project oc-uc1
+oc create project oc-uc2
+oc create project oc-uc3
+oc create project oc-uc4
+oc create project oc-uc5
+oc create project oc-uc6
+oc create project oc-uc7
+oc create project oc-uc8
+
